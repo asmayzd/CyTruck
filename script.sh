adaa96a268750ed973d6c60ca5b9d24649fcbe94
asmayzd <asmayazidi19@gmail.com>
2024-01-07 20:14:02 +0100

    Correction de la fonction d1 dans script.sh pour faire le graphique

--- script.sh
+++ script.sh
@@ -7,27 +7,44 @@ chmod +x traitement/d1
 d1() {
     
     # Compter le nombre de trajets pour chaque conducteur
-    awk -F';' '{ conducteurs[$6]++ } END { for (diver in conducteurs) print conducteurs[diver], diver }' data/test1.csv | sort -nr | head -n10 > temp/temp_d1.csv
+     awk -F";" '/;1;/ {compteur[$6] += 1} END {for (nom in compteur) print compteur[nom], nom}' data/test1.csv | sort -nrk1,1 | head -10 > temp/temp_d1.csv
 
     cat temp/temp_d1.csv
 
     # Créer un fichier de données pour l'histogramme
-    # awk -v OFS=';' -F';' '{print $2, $1}' temp/temp_d1.csv > temp/histogram_data.csv
+     awk -v OFS=';' -F';' '{print $2, $1}' temp/temp_d1.csv > temp/histogram_data.csv
 
-    # Générer l'histogramme horizontal avec gnuplot
-    # gnuplot <<-EOF
-    #     set terminal png size 1500,600
-    #     set output 'images/histogram_d1.png'
-    #     set title "Top 10 des conducteurs avec le plus de trajets"
-    #     set xlabel "Nombre de trajets"
-    #     set ylabel "Noms des conducteurs"
-    #     set style data histograms
-    #     set style fill solid border -1
-    #     set yrange [0:300]
-    #     set xrange [0:10] reverse
-    #     set boxwidth 0.8
-    #     plot 'temp/histogram_data.csv' using 1:xtic(2) notitle
-    # EOF
+#Partie graphique (gnuplot).
+        gnuplot << EOF
+
+        # Paramètres de sortie
+        set terminal pngcairo enhanced font 'Arial,10'
+        set output 'images/graphique_d1.png'
+
+        # Paramètres du graphique
+        set bmargin 13    # Ajuster la marge inférieure (en unités par défaut)
+        set rmargin 10
+        set lmargin 2
+        set title 'Top 10 des conducteurs avec le plus de trajets'
+        set xlabel 'Noms des conducteurs'
+        set ylabel 'Nombre de trajets'
+        set xtic rotate by 90 offset 0,-9
+        set xlabel rotate by 180 offset 0,-9
+        set ylabel offset 92,0
+        set ytic offset 83,0
+        set yrange [0:250]
+        set ytic rotate by 90
+        set style histogram rowstacked
+        set style fill solid border -1
+        set boxwidth 0.5
+        unset key
+
+        # Tracé du graphique
+        set datafile separator ';'
+        plot 'temp/histogram_data.csv' using 2:xtic(1) with boxes lc rgb 'blue'
+
+EOF
+
 }
 
 # Appel de la fonction d1Flag
