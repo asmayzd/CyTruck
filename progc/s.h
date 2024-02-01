fe6a924bf4aa0c346f4462814430ee495d2c1010
Badrr212 <158482563+Badrr212@users.noreply.github.com>
2024-02-02 00:54:59 +0100

    Update s.h

--- progc/s.h
+++ progc/s.h
@@ -4,4 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+Distance* Newdistance(float dist);
+AVL* createAVL(int routeNbr, float dist);
+AVL* createAVL2(int routeNbr, float maxdist, float mindist, float moyenne, float moyennedist);
+int taille(AVL* p);
+int getEquilibre(AVL* p);
+AVL* rotationDroite(AVL* p);
+AVL* rotationGauche(AVL* p);
+AVL* insertAVL(AVL* p, int routenbr, float dist);
+AVL* insertDistance(AVL* p2, int numid, float maxdist, float mindist, float moyenne, float moyennedistance);
+AVL* insertAVL2(AVL* p, AVL* p2);
+void displayInfixeInv(AVL* p);
+void destroyAVL(AVL* p);
